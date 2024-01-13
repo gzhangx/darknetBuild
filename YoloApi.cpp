cf9fc44caa76da08790c961b6c1fd2cdc3fab471
Gang Zhang <gzhangx@hotmail.com>
2024-01-12 23:34:43 -0500

    copy data and free

--- YoloApi.cpp
+++ YoloApi.cpp
@@ -1,6 +1,7 @@
 //https://github.com/micbelgique/YoloCSharp/blob/master/src/YoloApi/YoloApi.cpp
 #include "yolo_v2_class.hpp"
 #include <opencv2/opencv.hpp>
+#include<malloc.h>
 
 #if defined(_WINDOWS)
 #define YOLOAPI_EXPORT __declspec(dllexport)
@@ -43,10 +44,30 @@ extern "C" {
 	 * @param elems Returned bounding boxes of each detected object on an image (out)
 	 * @param array_size Amount of objects detected on the image (out)
 	 */
-	YOLOAPI_EXPORT void detect(unsigned char* mat_data, int mat_rows, int mat_cols, float thresh, bool use_mean, bbox_t** elems, int* elems_size) {
-		cv::Mat img_mat = cv::Mat(mat_rows, mat_cols, CV_8UC3, mat_data);
-		std::vector<bbox_t> result_vector = instance->detect(img_mat, thresh, use_mean);
-		*elems = result_vector.data();
+	YOLOAPI_EXPORT bbox_t* detect(cv::Mat *img_mat, float thresh, bool use_mean, int* elems_size) {
+		//cv::Mat img_mat = cv::Mat(mat_rows, mat_cols, CV_8UC3, mat_data);		
+		printf("element size passed in =%i\n", *elems_size);
+
+		std::vector<bbox_t> result_vector = instance->detect(*img_mat, thresh, use_mean);
+		//*elems = result_vector.data();
+
+		int copyLen = result_vector.size();
+		printf("Done, result size =%i, element size =%i\n", copyLen, sizeof(bbox_t));
+		bbox_t* elems = (bbox_t *)malloc(sizeof(bbox_t) * copyLen);
+		printf("malloc %llux%i %i\n", sizeof(bbox_t), copyLen, (int)elems);
+		if (elems != NULL) {
+			for (int i = 0; i < copyLen; i++) {
+				elems[i].h = 0;				
+				memcpy(elems + i, result_vector.data() + i, sizeof(bbox_t));
+				//elems[i] = result_vector[i];
+			}
+		}
 		*elems_size = result_vector.size();
+		printf("Done, result len %llu\n", result_vector.size());
+		return elems;
+	}
+
+	YOLOAPI_EXPORT void freeDetectData(bbox_t* data) {
+		free(data);
 	}
 }
